0fdb4003ebb34d39ac6be147aeacda8add1833b5
Gilberto Villa <ingilniero@gmail.com>
2013-06-23 18:31:51 -0500

    Add Book feature and steps definition

---
+++ features/step_definitions/book_steps.rb
@@ -0,0 +1,75 @@
+When(/^I fill the new book form with valid data$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the book should be added to database$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see it on library page$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill the new book form with empty title$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the book should not be added to database$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the new book form with error message$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^book "(.*?)" exists$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go to "(.*?)" book page$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" book details$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" book edit link$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go to nonexistent book page$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be redirected to library page$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" error message$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I change book title to "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^book "(.*?)" should not exist in database$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^book "(.*?)" should exist in database$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" book on library$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I delete it$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see "(.*?)" book on library page$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
