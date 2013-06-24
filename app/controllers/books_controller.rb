98e9a7a00f795436c12771bb2ec736360590b1f5
Gilberto Villa <ingilniero@gmail.com>
2013-06-24 01:06:35 -0500

    Add #create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,19 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new book_params
+    if @book.save
+      redirect_to books_url, notice: "You added a new book into your library"
+    else
+      flash.now[:error] = "Book without a title is pretty useless"
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit!
+  end
 end
